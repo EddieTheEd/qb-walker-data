819dbb204e837655cde832e173e11e53d51fdbb1
edwardhuynh <edwardleehuynh@gmail.com>
2026-04-16 15:24:07 +1000

    a

---
+++ batchmove.sh
@@ -0,0 +1,12 @@
+for i in $(seq 0 999); do
+  for f in science/science-$i-*; do
+    [ -e "$f" ] || continue
+    if (( i % 2 == 0 )); then
+      git mv "$f" science1/
+    else
+      git mv "$f" science2/
+    fi
+  done
+done
+
+git commit -m "Move files batch 0-999"
